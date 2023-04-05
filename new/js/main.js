69b1fdba03a0c6dbf94468276c5530013c4b9aa5
Paul Brain <paul_brain@mail.ru>
2023-04-05 19:18:10 +0300

    Modul 4 lesson 2: some improvements

--- new/js/main.js
+++ new/js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const cssConsole = 'padding: 0 3px; text-shadow: 1px 1px 2px #57fc7a';
+let cssConsole = 'padding: 0 3px; text-shadow: 1px 1px 2px #57fc7a'; // OK
 
-let productName = 'Samsung Galaxy A53';
-let productCount = 5;
-let productCategory = 'Mobile phones';
-let productPrice = 250;
+const productName = 'Samsung Galaxy A53';
+const productCount = 5;
+const productCategory = 'Mobile phones';
+const productPrice = 250;
 
 console.log('Имя товара:', productName);
 console.log('Общая сумма:', productPrice * productCount, '$');
@@ -17,5 +17,11 @@ console.log('Общая сумма:', productPrice * productCount, '$');
   const productPrice = + prompt("Введите цену товара: ");
 
   console.log(typeof productName, typeof productCount, typeof productCategory, typeof productPrice);
-  console.log(`На складе ${productCount} единицы товара ${productName} на сумму ${productPrice * productCount} руб.`);
+
+  if (Number.isNaN(productCount) || Number.isNaN(productPrice)) {
+    cssConsole = 'padding: 0 3px; text-shadow: 0 0 2px red'; // Error
+    console.log(`%cКажется вы ввели неверные данные`, cssConsole);
+  } else {
+    console.log(`%cНа складе ${productCount} единиц(ы) товара ${productName} на сумму ${productPrice * productCount} руб.`, cssConsole);
+  }
 }
