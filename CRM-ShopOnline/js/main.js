4cf32cec6f046a73337fe48b2c39ab9fc5480d44
Paul Brain <paul_brain@mail.ru>
2023-04-05 10:19:17 +0300

    Modul 4 lesson 1 has been done: created main.js

---
+++ CRM-ShopOnline/js/main.js
@@ -0,0 +1,7 @@
+let productName = 'Samsung Galaxy A53';
+let productCount = 5;
+let productCategory = 'Mobile phones';
+let productPrice = 250;
+
+console.log('Имя товара:', productName);
+console.log('Общая сумма:', productPrice * productCount, '$');
