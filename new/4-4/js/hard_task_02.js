127af390165f9b91efa4d94ae624c792cdb35773
Paul Brain <paul_brain@mail.ru>
2023-04-11 18:40:17 +0300

    Modul 4 lesson 4 hard task 2 changed some strings

--- new/4-4/js/hard_task_02.js
+++ new/4-4/js/hard_task_02.js
@@ -6,7 +6,13 @@
   const number1 = prompt("Введите первое число:");
   const number2 = prompt("Введите второе число:");
 
-  const numberMin = (a, b) => (a - b) < 0 ? a : b;
+  const numberMin = (a, b) => {
+    let min = a;
+
+    (a > b) && (min = b);
+
+    return min;
+  };
 
   alert('Минимальное из двух: ' + numberMin(number1, number2));
 }
