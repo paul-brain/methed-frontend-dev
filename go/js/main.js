74a77498c8477977c5e98d02b0da497010902417
Paul Brain <paul_brain@mail.ru>
2023-06-16 16:18:05 +0300

    Modul 6-4 ready: burger menu in Go project

--- go/js/main.js
+++ go/js/main.js
@@ -1,3 +1,5 @@
+
+/* Частые вопросы: реализация аккордиона */
 const faqListBtns = document.querySelectorAll('.faq__list-item-btn');
 const faqListTexts = document.querySelectorAll('.faq__list-item-text-wrapper');
 
@@ -20,24 +22,71 @@ faqListBtns.forEach((btn, btnIndex) => {
   });
 });
 
-const faq = document.querySelector('.faq');
-faq.addEventListener('click', ({target}) => {
-  console.log(target);
-}, true);
-
+/* Кнопка "Заказать звонок": открыть/закрыть модальное окно */
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 const callBtn = document.querySelector('.header__btn');
 
 callBtn.addEventListener('click', () => {
   overlay.classList.add('overlay--show');
-  modal.style.opacity = 1;
 });
 
 overlay.addEventListener('click', ({target}) => {
-  if (target.classList.contains('modal__btn-close') || target === overlay) {
+  if (
+    target.classList.contains('modal__btn-close')
+    || target === overlay
+    ) {
     overlay.classList.remove('overlay--show');
-    modal.style.opacity = 0;
   }
+});
 
+/* Кнопка "Бургер": открыть/закрыть меню */
+const burgerBtn = document.querySelector('.header__nav-btn');
+const headerMenu = document.querySelector('.header__nav-list');
+let scale = 0;
+
+const showMenu = () => {
+  scale += 0.05;
+  headerMenu.style.transform = `scale(${scale})`;
+
+  if (scale < 1) {
+    requestAnimationFrame(showMenu);
+  } else {
+    scale = 0;
+  }
+};
+
+// Открываем/закрываем меню по кнопке с анимацией
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('header__nav-btn--close');
+  headerMenu.classList.toggle('header__nav-list--open');
+
+  if (headerMenu.classList.contains('header__nav-list--open')) {
+    showMenu();
+  } else {
+    headerMenu.style.transform = `scale(0)`;
+  }
+});
+
+document.body.addEventListener('click', ({target}) => {
+  if (
+    ! target.classList.contains('header__nav-btn')
+    && ! target.classList.contains('header__nav-list')
+    && ! target.parentElement.classList.contains('header__nav-item')
+    ) {
+    burgerBtn.classList.remove('header__nav-btn--close');
+    headerMenu.classList.remove('header__nav-list--open');
+  }
+});
+
+/* Перемещаем кнопку "Заказать звонок" внутрь меню на мобильной версии */
+window.addEventListener('DOMContentLoaded', () => {
+  if (document.documentElement.scrollWidth <= 768) {
+    const li = document.createElement('li');
+
+    li.classList.add('header__nav-item');
+    li.append(callBtn);
+    headerMenu.append(li);
+    callBtn.style.display = 'inline-block';
+  }
 });
